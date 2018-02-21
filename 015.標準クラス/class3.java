33c1279a63961c9eef86fa8191af9693762b0a69
msysk5 <yusa.kei.8912@gmail.com>
2018-02-21 17:27:20 +0900

    課題　標準クラス

---
+++ 015.標準クラス/class3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Javaclass;
+
+/**
+ *
+ * @author guest1Day
+ */
+import java.util.Date;
+public class class3 {
+    public static void main(String[] args){
+    
+    Date day = new Date(1451574000000L);
+    System.out.print(day.getTime());
+    }
+}
