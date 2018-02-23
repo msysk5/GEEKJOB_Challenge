1e727487e800c700bf4ac8e542c10aa31067fc1b
msysk5 <yusa.kei.8912@gmail.com>
2018-02-23 17:41:01 +0900

    課題　標準クラス

---
+++ 015.標準クラス/class9.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Javaclass;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class class9 {
+ public static void main(String[] args){
+   String word1 = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+   String word2 = word1.replace("U","う");
+   System.out.print(word2.replace("I","い"));
+ 
+ }   
+}
